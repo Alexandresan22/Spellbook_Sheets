49ad109d71b7604b96f0d47200672c9795372087
Alexandre Santos <hectoramaciesza@gmail.com>
2025-02-05 16:28:51 -0300

    restruturação global do projeto
    Iniciando projeto do zero com vite, implementação do firebase descartando json-server

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+          <h2>É os de verdade, não tem jeito</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
